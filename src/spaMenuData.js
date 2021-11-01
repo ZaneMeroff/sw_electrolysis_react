89064ce664ab00a4a71ccddd1d3aacb143619f09
ZaneMeroff <zanemeroff@gmail.com>
2021-10-31 20:48:19 -0600

    update(spaMenuData.js): add content

--- src/spaMenuData.js
+++ src/spaMenuData.js
@@ -18,5 +18,30 @@ export const spaMenuData = {
       desc: 'Microneedling is a dermaroller procedure that uses small needles to prick the skin.', 
       price: 200 
     },
+    { 
+      name: 'Dermaplaning Face', 
+      desc: 'Exfoliate the epidermis by ridding the skin of fine vellus hair.', 
+      price: 35 
+    },
+  ],
+  'Peels & Lifts': [
+    { 
+      name: 'O2 Lift', 
+      desc: 'Luxurious oxygen treatment infuses plan-derived stem cells, peptides, and botanicas into the skin.', 
+      price: 90,
+      pkgPrice: '$248 (3x)'
+    },
+    { 
+      name: 'Vital C Lift', 
+      desc: 'Vitamin C enzymes speed up cell turnover. See results after one session.', 
+      price: 75,
+      pkgPrice: '$188 (3x)'
+    },
+    { 
+      name: 'Anti-Aging Lift', 
+      desc: 'A customized peel starting with a retinol, lactic, or salicylic base then adding medical grade enhancers.', 
+      price: 85,
+      pkgPrice: '$213 (3x)'
+    },
   ],
 }
