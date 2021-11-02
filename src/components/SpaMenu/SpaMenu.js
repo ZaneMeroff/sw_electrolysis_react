0c79b29b36061378a92dc5c263eeb5081e7396b7
ZaneMeroff <zanemeroff@gmail.com>
2021-11-01 21:24:01 -0600

    update(SpaMenu): update data, refactor css

--- src/components/SpaMenu/SpaMenu.js
+++ src/components/SpaMenu/SpaMenu.js
@@ -27,31 +27,17 @@ export const SpaMenu = () => {
                 {
                   menu[section].map((item, index) => {
                     return (
-
-                      <div key={index} className='menu-item-container' style={{ display: 'flex', height: 'auto', width: '400px', border: '1px solid red' }}>
-                        <div style={{ display: 'flex', flexDirection: 'column', width: '300px'}}>
-                          <div className='menu-item-title' style={{ height: 'auto', width: '100%', border: '1px solid blue' }}>{item.name}</div>
-                          <div className='menu-item-desc' style={{ height: 'auto', width: '100%', border: '1px solid green' }}>{item.desc}</div>
+                      <div key={index} className='menu-item-container' style={{ display: 'flex', height: 'auto' }}>
+                        <div style={{ display: 'flex', flexDirection: 'column', width: '100%'}}>
+                          <div className='menu-item-title'>{item.name}</div>
+                          <div className='menu-item-desc'>{item.desc}</div>
                         </div>
-                        <div style={{ display: 'flex', flexDirection: 'column', paddingLeft: '6px', border: '1px solid yellow' }}>
-                          <div className='menu-item-price' style={{ height: 'auto', border: '1px solid blue' }}><i>${item.price}</i></div>
-                          <div className='menu-item-price' style={{ height: 'auto', border: '1px solid green' }}><i>{ item.pkg1Price }</i></div>
-                          <div className='menu-item-price' style={{ height: 'auto', border: '1px solid yellow' }}><i>{ item.pkg2Price }</i></div>
+                        <div style={{ display: 'flex', flexDirection: 'column', paddingLeft: '10px' }}>
+                          <div className='menu-item-price'><i>${item.price}</i></div>
+                          {item.pkg3xPrice && <div className='menu-item-price'><i>${ item.pkg3xPrice } (3x)</i></div>}
+                          {item.pkg6xPrice && <div className='menu-item-price'><i>${ item.pkg6xPrice } (6x)</i></div>}
                         </div>
                       </div>
-
-                      // <table key={index} className='menu-item-container'>
-                      //   <tbody>
-                      //     <tr>
-                      //       <td className='menu-item-title'>{item.name}</td>
-                      //       <td className='menu-item-price'><i>${item.price}</i></td>
-                      //     </tr>
-                      //     <tr>
-                      //       <td className='menu-item-desc'>{item.desc}</td>
-                      //       <td className='menu-item-pkg-price'><i>{ item.pkg1Price }</i></td>
-                      //     </tr>
-                      //   </tbody>
-                      // </table>
                     )
                   })
                 }
